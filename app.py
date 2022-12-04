01e64ccf42ed4a3b5ad36133dd73c3838bced137
Ryan Shepard <shayde22@yahoo.com>
2022-12-03 19:35:29 -0600

    bot reworkings

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import discord
 from time import sleep
 from discord.ext import commands
 
+MY_GUILD = discord.Object(id='950937956682698862')
+
 class Tickler(commands.Bot):
     def __init__(self) -> None:
         super().__init__(command_prefix="||", intents=discord.Intents.all())
@@ -12,9 +14,11 @@ class Tickler(commands.Bot):
         print('Ticklers ready!')
 
     async def setup_hook(self) -> None:
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
         for f in os.listdir('./cogs'):
             if f.endswith('.py'):
                 await self.load_extension(f'cogs.{f[:-3]}')
 
 client = Tickler()
-client.run('MTA0ODQyNjgwNjgxNjAxNDM0Nw.GWgtjm.6Yi55Ezfx4qnXmJH5RNW1pNhRbLpDd9N-dN_jA')
+client.run('MTA0ODQyNjgwNjgxNjAxNDM0Nw.GYTaJw.CZy_JtlFzKEKT07Gb_TlJQjf7a6ex7OL-_sJUM')
