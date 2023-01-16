009349bfb9d892a28817401772d72047b46fdd7e
Ryan Shepard <shayde22@yahoo.com>
2023-01-16 17:58:42 -0600

    Tickle slash command

--- app.py
+++ app.py
@@ -11,14 +11,12 @@ class Tickler(commands.Bot):
         super().__init__(command_prefix="!", intents=discord.Intents.all())
 
     async def on_ready(self):
-        print('Ticklers ready!')
+        print('Ticklers ready!') 
 
     async def setup_hook(self) -> None:
-        self.tree.copy_global_to(guild=MY_GUILD)
-        await self.tree.sync(guild=MY_GUILD)
         for f in os.listdir('./cogs'):
             if f.endswith('.py'):
                 await self.load_extension(f'cogs.{f[:-3]}')
 
 client = Tickler()
-client.run('MTA0ODQyNjgwNjgxNjAxNDM0Nw.GFRVPA.ZYHnpGsd5DIVi_li287P5y-CeBj14sK5CRyajo')
+client.run('MTAxNzE2NDgyNjAyOTYwNDk0NQ.GV-a0X.KTakS1DVNgAoNwr2ljLjYTt53CsaCJOW6D1gq0')
--- cogs/tickle.py
+++ cogs/tickle.py
@@ -3,16 +3,23 @@ from time import sleep
 
 from discord.ext import commands
 
+MY_GUILD = discord.Object(id='950937956682698862')
+
 class Tickle(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
         
-    @commands.command(name='tickle', alias="t")
+    @commands.hybrid_command(name='tickle' aliases=["t"])
     @commands.has_role('new role')
     async def tickle(self, ctx, duration: int):
         for x in range(0, duration):
             sleep(1)
             await ctx.send('Tickle\n')
+    
+    @commands.command(name="sync")
+    async def sync(self, ctx):
+        ctx.bot.tree.copy_global_to(guild=MY_GUILD)
+        await ctx.bot.tree.sync(guild=MY_GUILD)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Tickle(bot))
